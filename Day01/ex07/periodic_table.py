058dcdfebc532e82301ca53bcefae2a34c8255d8
Macbook PRO <macbookpro@MacBook-Pro-Macbook.local>
2020-02-05 15:30:08 +0200

    [UPD] ex07

--- Day01/ex07/periodic_table.py
+++ Day01/ex07/periodic_table.py
@@ -1,5 +1,6 @@
 
-html_head = "<!DOCTYPE html>\n" \
+def html_head():
+    return "<!DOCTYPE html>\n" \
             "<html lang=\"en\">\n" \
             "   <head>\n" \
             "       <meta charset=\"utf-8\">\n" \
@@ -9,50 +10,22 @@ html_head = "<!DOCTYPE html>\n" \
             "   <body>\n"
 
 
-def for_better_displaying(elem_dict, counter_for_even):
-    if elem_dict['position'] == '0':
-        counter_for_even += 1
-    if counter_for_even % 2 != 0:
-        elem_dict['even'] = True
-    else:
-        elem_dict['even'] = False
-    return elem_dict, counter_for_even
-
-
-def read_file():
-    f = open("periodic_table.txt", 'r')
-    data = f.readline()
-    list_of_lines = []
-    counter_for_even = 1
-    while data:
-        elem_dict = {}
-        tmp_lst = data.split('=')
-        elem_dict['name'] = tmp_lst[0].strip()
-        for item in tmp_lst[1].split(','):
-            key, value = item.strip().split(':')
-            elem_dict.setdefault(key, value)
-        elem_dict, counter_for_even = for_better_displaying(elem_dict=elem_dict,
-                                                            counter_for_even=counter_for_even)
-        list_of_lines.append(elem_dict)
-        data = f.readline()
-    f.close()
-    make_html(list_of_lines)
-    # print(list_of_lines)
-
-
 def create_css_file():
     css_file = open("periodic_table.css", 'w+')
-    styles = "td {\n" \
+    styles = "h4 {\n" \
+             "margin-top: 3px;\n" \
+             "margin-bottom: 5px;\n" \
+             "padding-right: 7px;\n" \
+             "}\n" \
+             "td {\n" \
              "  border: 1px solid black;\n" \
              "  padding: 5px;\n" \
-             "}\n" \
-             ".even_elem {\n" \
-             "  text-align: right;\n" \
              "}\n" \
              ".sign {\n" \
              "text-align: right;\n" \
              "font-size: 1.3vw;\n" \
              "font: bold;\n" \
+             "padding-bottom: 3px;" \
              "}\n" \
              ".number {\n" \
              "text-align: left;\n" \
@@ -61,59 +34,73 @@ def create_css_file():
              ".massa {\n"\
              "text-align: right;\n" \
              "font-size: 0.75vw;\n" \
-             "font-weight: 10;\n" \
              "}\n" \
              ".electrons {\n" \
-             "  writing-mode: vertical-rl;\n" \
-             "  text-orientation: upright;\n" \
-             "  letter-spacing: 2px;\n" \
+             "display: block;\n" \
+             "justify-content: center;\n" \
+             "font-size: 10px;\n" \
+             "width: .9em;\n" \
+             "text-align: left;" \
+             "}\n" \
+             ".container {\n" \
+             "display: flex;\n" \
+             "justify-content: space-between;\n" \
+             "min-width: 60px;\n" \
+             "}\n" \
+             ".other_data {\n" \
+             "display: flex;\n" \
+             "flex-direction: column;\n" \
+             "text-align: left;\n" \
              "}\n"
     css_file.write(styles)
     css_file.close()
 
 
-def make_first_row(table):
-    for i in range(7):
-        table += "              <td>{col_number}</td>\n".format(col_number=i + 1)
-    table += "          </tr>\n" \
-             "          <tr>\n"
-    return table
+def read_file():
+    with open("periodic_table.txt", 'r') as f:
+        data = f.readline()
+        list_of_lines = []
+        while data:
+            elem_dict = {}
+            tmp_lst = data.split('=')
+            elem_dict['name'] = tmp_lst[0].strip()
+            for item in tmp_lst[1].split(','):
+                key, value = item.strip().split(':')
+                elem_dict.setdefault(key, value)
+            list_of_lines.append(elem_dict)
+            data = f.readline()
+    make_html(list_of_lines)
 
 
 def make_html(list_of_lines):
     html_file = open("periodic_table.html", "w+")
-    table = html_head + "       <table>\n" \
-                        "           <tr>\n"
-    table = make_first_row(table)
+    table = html_head() + "       <table>\n" \
+                          "           <tr>\n"
     for i in range(len(list_of_lines)):
         elem = list_of_lines[i]
+        col_space = 0
         if list_of_lines[i] != list_of_lines[-1]:
             col_space = int(list_of_lines[i + 1]['position']) - int(elem['position'])
         if elem['position'] == '0' and list_of_lines[0] != elem:
             table += "              </tr>\n" \
                      "              <tr>\n"
-        if elem['even'] is True:
-            elem_class = "even_elem"
-        else:
-            elem_class = ""
         table += "              <td>\n" \
                  "                  <div class=\"container\">\n" \
                  "                      <div class=\"other_data\">\n" \
-                 "                          <section class=\"number\">{number}</section>\n" \
+                 "                          <div class=\"number\">{number}</div>\n" \
                  "                          <h4>{name}</h4>\n" \
-                 "                          <section class=\"sign\">{small}</section>\n" \
-                 "                          <section class=\"massa\">{molar}</section>\n" \
+                 "                          <div class=\"sign\">{small}</div>\n" \
+                 "                          <div class=\"massa\">{molar}</div>\n" \
                  "                      </div>\n" \
-                 "                      <section class=\"electrons\">{electron}</section>\n" \
+                 "                      <div class=\"electrons\">{electron}</div>\n" \
                  "                  </div>\n" \
-                 "              </td>\n".format(elem_class=elem_class,
-                                                name=elem['name'],
+                 "              </td>\n".format(name=elem['name'],
                                                 number=elem['number'],
                                                 small=elem['small'],
                                                 molar=elem['molar'] ,
                                                 electron=elem['electron'])
         if col_space > 1:
-            table += "          <td colspan=\"{num}\">\n" \
+            table += "          <td colspan=\"{num}\" style=\"border: none;\">\n" \
                      "          </td>".format(num=col_space - 1)
     table += "          </tr>\n" \
              "      </table>\n" \
@@ -124,13 +111,5 @@ def make_html(list_of_lines):
 
 
 if __name__ == '__main__':
-    # create_css_file()
+    create_css_file()
     read_file()
-
-#
-# ".sign {\n" \
-#              "text-align: center;\n" \
-#              "font-size: 0.9vw;\n" \
-#              "font-weight: 50;\n" \
-#              "}\n" \
- # "  writing-mode: vertical-rl;\n" \
